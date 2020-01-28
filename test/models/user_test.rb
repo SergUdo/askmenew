1eb15d7499b616357e161001dd3217ccebf01416
SergUdo <sergii.udovichenko@gmail.com>
2020-01-28 23:05:11 +0200

    Add tests user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: "Example", username: "Vova", password: "123", email: "user@ex.com")
+  end
+
+  test "should be valid" do
+    assert_not @user.valid?
+  end
+
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "name should be saved as lower-case" do
+    mixed_case_name = "FootBooList"
+    @user.name = mixed_case_name.downcase
+    @user.save
+    assert_equal mixed_case_name.downcase, @user.name
+  end
 end
