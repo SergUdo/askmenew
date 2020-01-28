c4076364ccd46d707f623388392feb3731e6592e
SergUdo <sergii.udovichenko@gmail.com>
2020-01-28 22:38:36 +0200

    Add views and helper

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-get 'show' => 'users#show'
+
+root 'users#index'
+
+  # Эти две строчки добавляют ресурсы для пользователей и вопросов. Ресурс — это
+  # набор путей для управления соответствующей моделью. Посмотрите, что напишет
+  # команда
+  #
+  resources :users
+  resources :questions
 end
