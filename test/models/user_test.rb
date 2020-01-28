bca7709a6bdd7383ce540ceba6b3e419b048ad3d
SergUdo <sergii.udovichenko@gmail.com>
2020-01-29 00:05:17 +0200

    Update user model test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,11 +1,22 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+
   def setup
     @user = User.new(name: "Example", username: "Vova", password: "123", email: "user@ex.com")
   end
 
   test "should be valid" do
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = '  '
+    assert_not @user.valid?
+  end
+
+  test "name should not be too long" do
+    @user.name = "a" * 300
     assert_not @user.valid?
   end
 
@@ -24,8 +35,17 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "name should be saved as lower-case" do
-    mixed_case_name = @user.name.downcase
+    downcase_name = @user.name.downcase
     @user.save
-    assert_equal mixed_case_name, @user.name.downcase
+    assert_equal downcase_name, @user.name.downcase
+  end
+
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
   end
 end
