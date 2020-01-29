64e6d69bad416b0d4e0196cb25b2f74c8a68a0c4
SergUdo <sergii.udovichenko@gmail.com>
2020-01-29 17:04:26 +0200

    Add view session new

--- config/routes.rb
+++ config/routes.rb
@@ -12,11 +12,4 @@ Rails.application.routes.draw do
   # Ресурс вопросов (кроме экшенов :show, :new, :index)
   resources :questions, except: [:show, :new, :index]
 
-  # Синонимы путей — в дополнение к созданным в ресурсах выше.
-  #
-  # Для любознательных: синонимы мы добавили, чтобы показать одну вещь и потом
-  # их удалим.
-  get 'sign_up' => 'users#new'
-  get 'log_out' => 'sessions#destroy'
-  get 'log_in' => 'sessions#new'
 end
