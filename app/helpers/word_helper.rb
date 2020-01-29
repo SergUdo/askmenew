24810ab4d464565eea6b32ffe543b390960d6712
SergUdo <sergii.udovichenko@gmail.com>
2020-01-29 22:01:24 +0200

    Update

--- app/helpers/word_helper.rb
+++ app/helpers/word_helper.rb
@@ -1,10 +1,9 @@
 module WordHelper
-  def type_question
-    @number = @questions.count
-    "вопросов" if (@number % 100).between?(11, 14)
-    ostatok = @number % 10
-    return "вопрос" if ostatok == 1
-    return "вопроса" if ostatok.between?(2, 4)
-    return "вопросов" if ostatok.between?(5, 9) || ostatok.zero?
+  def type_question(number)
+    "#{number}вопросов" if (number % 100).between?(11, 14)
+    ostatok = number % 10
+    return "#{number}вопрос" if ostatok == 1
+    return "#{number} вопроса" if ostatok.between?(2, 4)
+    return "#{number} вопросов" if ostatok.between?(5, 9) || ostatok.zero?
   end
 end
