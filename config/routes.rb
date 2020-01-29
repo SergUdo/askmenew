b91b1ee861ce416545aac824841f2c4d25c776a8
SergUdo <sergii.udovichenko@gmail.com>
2020-01-29 18:13:14 +0200

    Change routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,8 @@ Rails.application.routes.draw do
   # Ресурс вопросов (кроме экшенов :show, :new, :index)
   resources :questions, except: [:show, :new, :index]
 
+
+  get 'sign_up' => 'users#new'
+  get 'log_out' => 'sessions#destroy'
+  get 'log_in' => 'sessions#new'
 end
