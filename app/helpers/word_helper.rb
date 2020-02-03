19560f3dc32d5fcbff0226ee4d8c8366b4478381
SergUdo <sergii.udovichenko@gmail.com>
2020-02-03 12:12:00 +0200

    Change helper view

--- app/helpers/word_helper.rb
+++ app/helpers/word_helper.rb
@@ -1,10 +1,9 @@
 module WordHelper
-  def type_question
-    @number = @questions.count
-    "вопросов" if (@number % 100).between?(11, 14)
-    ostatok = @number % 10
-    return "вопрос" if ostatok == 1
-    return "вопроса" if ostatok.between?(2, 4)
-    return "вопросов" if ostatok.between?(5, 9) || ostatok.zero?
+  def inclination(number, answer1, answer2, answer3)
+    return "#{number} #{answer3}" if (number % 100).between?(11, 14)
+    ostatok = number % 10
+    return "#{number} #{answer1}" if ostatok == 1
+    return "#{number} #{answer2}" if ostatok.between?(2, 4)
+    return "#{number} #{answer3}" if ostatok.between?(5, 9) || ostatok.zero?
   end
 end
