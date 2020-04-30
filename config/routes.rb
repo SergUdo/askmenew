d8d1957b8bbe8d79bc8be73a838e9872ff72612a
Sergii Udovichenko <sergii.udovichenko@gmail.com>
2020-04-30 21:21:47 +0300

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,8 @@ Rails.application.routes.draw do
 
   resources :questions, except: [:show, :new, :index]
 
+  get 'sign_up' => 'users#new'
+  get 'log_out' => 'sessions#destroy'
+  get 'log_in' => 'sessions#new'
+
 end
