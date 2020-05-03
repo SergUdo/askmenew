d341a78b401984c959b0f63b5a70cec051cb56ca
Sergii Udovichenko <sergii.udovichenko@gmail.com>
2020-05-03 21:46:38 +0300

    Change file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -61,7 +61,7 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation,
+    params.require(:user).permit(:id, :email, :password, :password_confirmation,
                                  :name, :username, :avatar_url)
   end
 end
