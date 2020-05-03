5646ddee3a7a086f567fecdffe86d757fc01be96
Sergii Udovichenko <sergii.udovichenko@gmail.com>
2020-05-03 19:42:30 +0300

    Update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   end
 
   def new
-    redirect_to user_path(@user), alert: 'Вы уже залогинены' if current_user.present?
+    redirect_to root_url, alert: 'Вы уже залогинены' if current_user.present?
     @user = User.new
   end
 
